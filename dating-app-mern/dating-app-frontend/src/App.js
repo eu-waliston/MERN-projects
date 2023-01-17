115c1bd6a0de013cfdf93206934bab303893ee2a
walwizard <goneisgone@hotmail.com>
2023-01-16 22:13:41 -0300

    Front Finalizado

--- dating-app-mern/dating-app-frontend/src/App.js
+++ dating-app-mern/dating-app-frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import DatingCards from './components/DatingCards';
+import SwipeButtons from "./components/SwipeButtons";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <DatingCards />
+      <SwipeButtons />
     </div>
   );
 }
