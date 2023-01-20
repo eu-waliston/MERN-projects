8457bd33156d853d395d551281bfaf5a0d51f577
walwizard <goneisgone@hotmail.com>
2023-01-20 10:17:43 -0300

    Video Side bar Icon finalizado

--- short-video-mern/short-video-frontend/src/components/Video.js
+++ short-video-mern/short-video-frontend/src/components/Video.js
@@ -1,6 +1,7 @@
 import React , {useRef, useState} from "react";
 import "./Video.css";
 import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
 
 const Video = () => {
   const [playing, setPlaying] = useState(false);
@@ -26,6 +27,7 @@ const Video = () => {
         onClick={handleVideoPress}
       ></video>
       <VideoFooter />
+      <VideoSidebar />
     </div>
   );
 };
---
+++ short-video-mern/short-video-frontend/src/components/VideoSidebar.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import './VideoSidebar.css'
+
+import FavoriteIcon from "@mui/icons-material/Favorite"
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import MessageIcon from '@mui/icons-material/Message'
+import ShareIcon from '@mui/icons-material/Share'
+
+const VideoSidebar = () => {
+    const [liked, setLiked] = useState(false);
+
+    return (
+        <div className="videoSidebar">
+            <div className="videoSidebar__button">
+                {liked ? <FavoriteIcon fontSize="large" onClick={e => setLiked(false)} /> : <FavoriteBorderIcon fontSize="large" onClick={e => setLiked(true)} />}
+
+                <p>
+                  {liked ? 101 : 100}  
+                </p>
+            </div>
+
+            <div className="videoSidebar__button">
+                <MessageIcon fontSize="large" />
+                <p>345</p>
+            </div>
+
+            <div className="videoSidebar__button">
+                <ShareIcon fontSize="large" />
+                <p>109</p>
+            </div>
+        </div>
+    )
+}
+export default  VideoSidebar;
