18bbfd72dd490f14b0b6ee8c62202cfdd5c7aeff
walwizard <goneisgone@hotmail.com>
2023-01-25 13:37:00 -0300

    Messaging App Frontend Sidebar

--- messaging-app-mern/messaging-app-frontend/src/components/Sidebar.js
+++ messaging-app-mern/messaging-app-frontend/src/components/Sidebar.js
@@ -5,13 +5,13 @@ import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import ChatIcon from "@mui/icons-material/Chat";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Avatar, IconButton } from "@mui/material";
-import SearchOutlined from "@mui/icons-material/SearchOutlined"
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar  src="https://pbs.twimg.com/profile_images/1495929577310494720/1rsOGIwP_400x400.jpg"/>
+        <Avatar src="https://pbs.twimg.com/profile_images/1495929577310494720/1rsOGIwP_400x400.jpg" />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
@@ -27,8 +27,10 @@ const Sidebar = () => {
         </div>
       </div>
       <div className="sidebar__search">
-        <SearchOutlined />
-        <input placeholder="Search os start new chat"  type="text"/>
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search os start new chat" type="text" />
+        </div>
       </div>
       <div className="sidebar__chats"></div>
     </div>
