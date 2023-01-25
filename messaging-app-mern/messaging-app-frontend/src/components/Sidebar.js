0d33499a369bae1810654943e7c4e593d27e0b79
walwizard <goneisgone@hotmail.com>
2023-01-25 09:56:31 -0300

    Messaging App Frontend

---
+++ messaging-app-mern/messaging-app-frontend/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className='sidebar__header'></div>
+            <div className='sidebar__search'></div>
+            <div className='sidebar__chats'></div>
+        </div>
+    )
+}
+
+export default Sidebar;
