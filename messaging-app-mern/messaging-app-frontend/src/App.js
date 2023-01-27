ff02cbe59746dd39995b76c9b661c943b8d98a8a
walwizard <goneisgone@hotmail.com>
2023-01-27 12:23:49 -0300

    Integration the PUSHER on the back and front ends

--- messaging-app-mern/messaging-app-frontend/src/App.js
+++ messaging-app-mern/messaging-app-frontend/src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Pusher from "pusher.js";
 
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 
 const App = () => {
+  const [messages, setMessages] = useState("");
+
+  useEffect(() => {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher("f6fcde04f278d879b96b", {
+      cluster: "us2",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      setMessages([...messages, data]);
+    });
+
+    return () => {
+      channel.unbind__all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
+
+
+
+
   return (
     <div className="app">
       <div className="app__body">
