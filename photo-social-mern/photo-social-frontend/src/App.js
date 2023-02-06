6dd5f9a27a60605036aa3ddc36290e8152862276
walwizard <goneisgone@hotmail.com>
2023-02-06 16:07:30 -0300

    MERN Firebase OAuht

--- photo-social-mern/photo-social-frontend/src/App.js
+++ photo-social-mern/photo-social-frontend/src/App.js
@@ -1,40 +1,78 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import { makeStyles } from '@mui/styles';
+import Modal from '@mui/material/Modal';
+import { Input, Button } from '@mui/material';
+
+import { auth } from './firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
 import Post from './components/post/Post';
 
-import { makeStyles } from '@mui/styles';
-
-import Modal from '@mui/material/Modal';
-import { Button, Input } from '@mui/material';
 
 function getModalStyle() {
-  const top = 50;
-  const left = 50;
+  const top = 40;
+  const left = 25;
   return {
     top: `${top}%`,
     left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
+    tranform: `translate(-${top}%, -${left}%)`,
   };
 }
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: 'azure',
     position: 'absolute',
     width: 400,
-    backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
+    boxShadow: '2px 2px 12px #0000002c',
+    padding: '2px 2px 2px 2px',
+    color: '#000'
+  }
+})
 
 function App() {
 
+  /* Styles Area */
   const classes = useStyles();
   const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
+  /*User  Area */
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  /*Auth Area*/
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        console.log(authUser);
+        setUser(authUser)
+      } else {
+        setUser(null)
+      }
+    })
+    return () => {
+      unsubscribe();
+    }
+  }, [user, username])
+
+  const signUp = async (e) => {
+    e.preventDefault();
+
+    await auth.createUserWithEmailAndPassword(email, password).then(authUser => authUser.user.updateProfile({
+      displayName: username
+    })).catch(error => alert(error.message))
+    setOpen(false);
+  }
+
+
+  /*Posts Area */
   const [posts, setPosts] = useState([
     {
       username: "FB_Shaulyb",
@@ -51,20 +89,53 @@ function App() {
   return (
     <div className="App">
       <Modal open={open} onClose={() => setOpen(false)}>
-        <div style={modalStyle} className=
-          {classes.paper}>
-          <h2>Modal Code</h2>
+        <div style={modalStyle} className={classes.root}>
+
+          <form className='app__signup'>
+            <center>
+              <img className='app__headerImahe' alt='Header' src={require("./components/assets/logo.png")} />
+            </center>
+
+            <Input
+              placeholder='username'
+              type='text'
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+
+            <Input
+              placeholder='email'
+              type='text'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+
+            <Input
+              placeholder='password'
+              type='password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+
+            <Button type='submit' onClick={signUp}>
+              Sign Up
+            </Button>
+          </form>
+
         </div>
       </Modal>
-
 
       <div className='app__header'>
         <img src={require("./components/assets/logo.png")} alt="Header" className="app__headerImage" />
       </div>
 
-      <Button onClick={() => setOpen(true)}>
-        Sign Up
-      </Button>
+
+      {
+        user ?
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+        :
+        <Button onClick={() => setOpen(true)}></Button>
+      }
 
       {posts.map(post => (
         <Post
