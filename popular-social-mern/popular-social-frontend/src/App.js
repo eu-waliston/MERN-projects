69e3a9b705bf9e9748ebc11be36cbcf74ab648f1
walwizard <goneisgone@hotmail.com>
2023-02-08 14:42:20 -0300

    Social Media App Header Component

--- popular-social-mern/popular-social-frontend/src/App.js
+++ popular-social-mern/popular-social-frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import styled from 'styled-components';
+
 function App() {
   return (
     <div className="App">
-      <h1>Popular Social Network MERN</h1>
+      <AppWrapper>
+        <h1>Popular Social Network MERN</h1>
+      </AppWrapper>
     </div>
   );
 }
-
+const AppWrapper = styled.div `
+  background-color: #f1f2f5;
+`
 export default App;
