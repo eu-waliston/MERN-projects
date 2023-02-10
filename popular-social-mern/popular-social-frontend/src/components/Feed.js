ab67341b270641a264fc85411af8210e73117446
walwizard <goneisgone@hotmail.com>
2023-02-10 13:53:53 -0300

    Post Component

--- popular-social-mern/popular-social-frontend/src/components/Feed.js
+++ popular-social-mern/popular-social-frontend/src/components/Feed.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Stories from './Stories';
 import Messenger from './Messenger'
+import Post from './Post';
 
 import styled from 'styled-components';
 
@@ -10,11 +11,26 @@ const Feed = () => {
         <FeedWrapper>
             <Stories />
             < Messenger />
+            <Post
+                profilePic="https://pngimg.com/uploads/anime_girl/anime_girl_PNG96.png"
+                timestamp="1609512232424"
+                imgName="https://s2.best-wallpaper.net/wallpaper/3840x2160/1901/Anime-girl-rose-thorns_3840x2160.jpg"
+                username="Rose Thorns"
+                message="I'm in love with this pic 🔥"
+            />
+
+            <Post
+                profilePic="https://pngimg.com/uploads/anime_girl/anime_girl_PNG75.png"
+                timestamp="1509512232424"
+                imgName="https://www.yummytabs.com/assets/anime/backgrounds/default.jpg"
+                username="Yumy Tabs"
+                message="I'm so excited with this season 😍😍😍"
+            />
         </FeedWrapper>
     )
 }
 
-const FeedWrapper = styled.div `
+const FeedWrapper = styled.div`
     flex:1;
     padding: 30px 150px;
     display: flex;
